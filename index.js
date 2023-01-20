c1fa26e106caf8917c1ef28a16a83ed96d135805
Miguel Camilo <miguelcamilo48@gmail.com>
2023-01-19 23:42:23 -0500

    instead of .then .catch using await & async to mutate the database

--- index.js
+++ index.js
@@ -1,49 +1,58 @@
 // import firebase admin library, cert allows you to connect to firebase
 import { initializeApp, cert } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore"
+import { getFirestore } from "firebase-admin/firestore";
 // import credentials
-import { service_account } from "./secrets.js"
- 
+import { service_account } from "./secrets.js";
+
 // connect to firebase project
 initializeApp({
-    credential: cert(service_account)
-})
+	credential: cert(service_account),
+});
 
 // connect to firestore database
-const db = getFirestore()
+const db = getFirestore();
 
 // create our object
 const food = {
-    name: 'Grilled Cheese',
-    style: 'American Style',
-    feeds: 1,
-    price: 10.99,
-    meal: 'Appetizer',
-    prepTime: '5 min',
-    inStock: true,
-    description: 'Cheesy'
-}
+	name: "Hamburger",
+	style: "American Style",
+	feeds: 1,
+	price: 5.99,
+	meal: "Lunch",
+	prepTime: "5 min",
+	inStock: true,
+	description: "Juicy",
+};
 
 // create a collection
 // go into db (database)
 // go into 'foods' collection
 // add a new document with data above (food)
+const addDoc = async (item) => {
+	const result = await db.collection("foods").add(item);
+	console.log(`Added Food --> ${result.id}`); // returns document id
+};
 
-db.collection('foods').add(food)
-
-    .then(doc => console.log(`Added Food --> ${doc.id}`)) // returns document id
-    .catch(console.error) // catching and display err
 
 // gets the data
-db.collection("foods").get()
+const getData = async () => {
+	const collection = await db.collection("foods").get();
 
-    .then(collection => {
-        
-        const foods = collection.docs.map(doc => {
-            let food = doc.data()
-            food.id = doc.id
-            return food
-        })
+	const foods = collection.docs.map((doc) => {
+		let food = doc.data();
+		food.id = doc.id;
+		return food;
+	});
+	console.table(foods);
+};
 
-        console.table(foods)
-    })
+// deletes document by id
+const deleteData = async (itemId) => {
+    const result = await db.collection('foods').doc(itemId).delete()
+    console.log(`Item has been deleted:  ${result.id}`)
+}   
+
+
+// await addDoc(food);
+await deleteData('bPUJmv5omELW4JWTnUIV');
+await getData();
